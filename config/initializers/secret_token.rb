42f8eeec83f057bae6d9f033b32705cb098814f6
Jason Sun <ysunbj@yahoo.com>
2014-05-25 22:44:51 +0800

    add rails getting started blog demo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '13e5d51ad870894b2e44c30704d65bf3c4351249d8b8597d1d7ddaf8fe5f41f9d3dcb4b60272a8d133d08deaf78270eb8a61b1f679eab3adacf98c35c321b955'
